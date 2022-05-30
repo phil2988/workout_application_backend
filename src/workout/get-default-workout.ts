2303f232071da572705f7a773176361f13fc33a7
phillipblicher <pbl@it-minds.dk>
2022-05-30 09:46:45 +0200

    added default workout endpoint

--- src/workout/get-default-workout.ts
+++ src/workout/get-default-workout.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import { Request, Response } from 'express';
 import { getPrisma } from "../utils/prisma";
 
-export async function getWorkout(request: Request, response: Response){
+export async function getDefaultWorkout(request: Request, response: Response){
     const prisma = await getPrisma();
 
     const result = await prisma.workout.findFirst({where: {default: true}, include: {exercises: true}});
