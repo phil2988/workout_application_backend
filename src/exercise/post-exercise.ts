3282457b443647b78648046b826d591f02c57ea2
phillipblicher <pbl@it-minds.dk>
2022-06-29 13:50:00 +0200

    ssss

--- src/exercise/post-exercise.ts
+++ src/exercise/post-exercise.ts
@@ -4,10 +4,16 @@ import { getPrisma } from "../utils/prisma";
 export async function postExercises(request: Request, response: Response){
     const prisma = await getPrisma();
 
-    const done = await prisma.exercise.create({data: request.body});
+    const done = await prisma.exercise.create({data: {
+        title: request.body.title,
+        description: request.body.description,
+        category: request.body.category,
+        parentExercise: request.body.parentExercise,
+        images: request.body.images
+    }});
 
     if(done)
-      return response.status(200);
+      return response.status(200).send(done);
     
-    return response.status(400);
+    return response.status(400).send(done);
 }
