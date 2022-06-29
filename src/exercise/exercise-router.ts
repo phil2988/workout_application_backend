df05decfc57fe97fc3f63d3115b08b57c4429bc8
phillipblicher <pbl@it-minds.dk>
2022-06-29 13:30:17 +0200

    fix i guess?

--- src/exercise/exercise-router.ts
+++ src/exercise/exercise-router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { getExercises } from './get-exercises';
+import { postExercises } from './post-exercise';
 
 
 export const exerciseRouter = Router();
 
 exerciseRouter.get('', getExercises);
+exerciseRouter.post('', postExercises);
